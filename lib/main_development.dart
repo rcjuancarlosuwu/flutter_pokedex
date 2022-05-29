f21d6281e307d31d74d9d3cdb59ca81c4a0ead12
Juan Carlos Ramón Condezo <rcjuancarlos01@gmail.com>
2022-05-28 19:35:21 -0500

    feat(pokedex): update main entry points for each enviroment

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:pokedex/bootstrap.dart';
 import 'package:pokedex/core/app/pokedex_app.dart';
+import 'package:pokedex_api/pokedex_api.dart';
 
-void main() {
-  bootstrap(() => const PokedexApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocalDataSource.init();
+  final pokemonRepository = PokemonRepository();
+
+  await bootstrap(
+    () => PokedexApp(
+      pokemonRepository: pokemonRepository,
+    ),
+  );
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,13 @@
 import 'package:pokedex/bootstrap.dart';
 import 'package:pokedex/core/app/pokedex_app.dart';
+import 'package:pokedex_api/pokedex_api.dart';
 
 void main() {
-  bootstrap(() => const PokedexApp());
+  final pokemonRepository = PokemonRepository();
+
+  bootstrap(
+    () => PokedexApp(
+      pokemonRepository: pokemonRepository,
+    ),
+  );
 }
